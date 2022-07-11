04ceb135558cc3e6973f1aa3920fba2caa8bdbd8
fsarter <fsarter@163.com>
2022-07-11 22:53:19 +0800

    work on aside menu

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="flex">
-    <div
-      :class="`flex ${
-        ['', 'w-4', 'w-8', 'w-12', 'w-16', 'w-20', 'w-24', 'w-28'][level]
-      }`"
-    ></div>
-    <span>{{ itemData.label }}</span>
+  <div class="my-menu-item" @click="clickMenuItem()">
+    <div class="flex">
+      <div
+        :class="`flex ${
+          ['', 'w-4', 'w-8', 'w-12', 'w-16', 'w-20', 'w-24', 'w-28'][level]
+        }`"
+      ></div>
+      <span>{{ itemData.label }}</span>
+    </div>
+    <div v-if="itemData.children && itemData.children.length > 0">
+      <SvgIcon
+        :name="
+          expand
+            ? 'keyboard_arrow_down_FILL0_wght400_GRAD0_opsz48'
+            : 'keyboard_arrow_left_FILL0_wght400_GRAD0_opsz48'
+        "
+        class="w-4 h-4"
+      />
+    </div>
   </div>
-  <MenuItem
-    v-for="item in itemData.children"
-    :key="item.key"
-    :item-data="item"
-    :level="level + 1"
-  />
+
+  <div v-if="expand">
+    <MenuItem
+      v-for="item in itemData.children"
+      :key="item.key"
+      :item-data="item"
+      :level="level + 1"
+    />
+  </div>
 </template>
 <script>
 export default {
@@ -29,5 +44,20 @@ export default {
       default: 0,
     },
   },
+  data() {
+    return {
+      expand: false,
+    };
+  },
+  methods: {
+    clickMenuItem() {
+      this.expand = !this.expand;
+    },
+  },
 };
 </script>
+<style lang="postcss" scoped>
+.my-menu-item {
+  @apply flex items-center justify-between h-12 cursor-pointer hover:bg-blue-100 hover:font-bold active:font-bold active:text-white active:bg-blue-600;
+}
+</style>
