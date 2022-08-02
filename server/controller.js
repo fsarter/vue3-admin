95b780e446ac0b2db706d9c2303243167de54623
fsarter <fsarter@163.com>
2022-08-02 22:31:45 +0800

    complete server api

--- server/controller.js
+++ server/controller.js
@@ -24,3 +24,46 @@ exports.user = {
     }
   },
 };
+
+exports.tableData = {
+  query: async (ctx) => {
+    const { rowId } = ctx.request.body;
+    const data = [
+      {
+        id: 'Row1',
+        column1: 'Value1.1',
+        column2: 'Value1.2',
+        column3: 'Value1.3',
+      },
+      {
+        id: 'Row2',
+        column1: 'Value2.1',
+        column2: 'Value2.2',
+        column3: 'Value2.3',
+      },
+      {
+        id: 'Row3',
+        column1: 'Value3.1',
+        column2: 'Value3.2',
+        column3: 'Value3.3',
+      },
+    ];
+    if (rowId) {
+      ctx.body = {
+        code: 0,
+        data: data.filter((row) => row.id === rowId),
+        msg: 'ok',
+      };
+    } else {
+      ctx.body = { code: 0, data: data, msg: 'ok' };
+    }
+  },
+  delete: async (ctx) => {
+    const { rowId } = ctx.request.body;
+    if (rowId) {
+      ctx.body = { code: 0, msg: `row ${rowId} delete success.` };
+    } else {
+      ctx.body = { code: 4002, msg: 'missing rowId.' };
+    }
+  },
+};
--- server/router.js
+++ server/router.js
@@ -6,5 +6,7 @@ const router = new Router();
 
 router.get('/', controller.home.welcome);
 router.post('/login', bodyParser(), controller.user.login);
+router.post('/table-data/query', bodyParser(), controller.tableData.query);
+router.post('/table-data/delete', bodyParser(), controller.tableData.delete);
 
 module.exports = router;
